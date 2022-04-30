0fe056a87609f4b1f2eccaa96ee904439a523153
Deepaksahnii <99683019+Deepaksahnii@users.noreply.github.com>
2022-04-30 16:47:14 +0530

    Lesson 46 completed

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const mongoose= require('mongoose');
 
 
-const mongoURI= "mongodb://localhost:27017/?readPreference=primary&directConnection=true&ssl=false";
+const mongoURI= "mongodb://localhost:27017/notesave?readPreference=primary&directConnection=true&ssl=false";
 
 const connectToMongo= ()=> {
     mongoose.connect(mongoURI, () => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,30 @@
 const express= require('express');
 const router= express.Router();
 const User= require('../models/User')
+const { body, validationResult } = require('express-validator');
+
+
+
 //Create a user using POST: Doesn't require Auth
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user= User(req.body);
-    user.save();
- res.send(req.body);
+router.post('/',[
+    body('name','Enter a valid name').isLength({min:3}),
+    body('email','Enter a valid Email').isEmail(),
+    body('password','Password should not be less than 5 characters').isLength({min:5})
+],(req, res)=>{
+ // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user))
+.catch(err=>{
+    console.log(err);
+    res.json({error: "Please Enter a unique value for email", message: err.message});
+})
 })
 
 module.exports= router;
